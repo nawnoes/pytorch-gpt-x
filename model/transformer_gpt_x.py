897913002dcaea8c37898785c3cd682f5eac3dca
nawnoes <nawnoes@gmail.com>
2021-05-19 22:17:55 +0900

    change optimizer add transformerGPTX

--- model/transformer_gpt_x.py
+++ model/transformer_gpt_x.py
@@ -4,12 +4,6 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.nn import CrossEntropyLoss
 
-"""
-self-Attention의 경우 Query Q, Key K, Value V를 입력으로 받아
-MatMul(Q,K) -> Scale -> Masking(opt. Decoder) -> Softmax -> MatMul(result, V)
-
-"""
-
 def self_attention(query, key, value, mask=None, causal=False):
   key_transpose = torch.transpose(key,-2,-1)                      # (bath, head_num, d_k, token_)
   matmul_result = torch.matmul(query,key_transpose)                # MatMul(Q,K)
@@ -31,17 +25,6 @@ def self_attention(query, key, value, mask=None, causal=False):
 
   return result, softmax_attention_score
 
-
-"""
-멀티헤드 어텐션
-MultiHead(Q,K,V) = Concat(head_1,head_2,...head_n)W^O
-head_i = Attention(QW^Q_i, KW^K_i, VW^V_i)
-W^Q는 모델의 dimension x d_k
-W^K는 모델의 dimension x d_k
-W^V는 모델의 dimension x d_v
-W^O는 d_v * head 갯수 x 모델 dimension
-논문에서는 헤더의 갯수를 8개 사용
-"""
 class MultiHeadAttention(nn.Module):
   def __init__(self, head_num =8 , d_model = 512,dropout = 0.1, causal=False):
     super(MultiHeadAttention,self).__init__()
@@ -84,12 +67,7 @@ class MultiHeadAttention(nn.Module):
 
     return self.w_o(attention_result)
 
-"""
-Position-wise Feed-Forward Networks
-FFN(x) = max(0,xW_1 + b_1)W_2+b2
-입력과 출력은 모두 d_model의 dimension을 가지고
-내부의 레이어는 d_model * 4의 dimension을 가진다.
-"""
+
 class FeedForward(nn.Module):
   def __init__(self,d_model, dropout = 0.1):
     super(FeedForward,self).__init__()
@@ -99,11 +77,7 @@ class FeedForward(nn.Module):
 
   def forward(self, x):
     return self.w_2(self.dropout(F.relu(self.w_1(x))))
-"""
-Layer Normalization
-: layer의 hidden unit들에 대해서 mean과 variance를 구한다. 
-nn.Parameter는 모듈 파라미터로 여겨지는 텐서
-"""
+
 class LayerNorm(nn.Module):
   def __init__(self, features, eps=1e-6):
     super(LayerNorm,self).__init__()
@@ -125,11 +99,6 @@ class ResidualConnection(nn.Module):
   def forward(self, x, sublayer):
     return x + self.dropout((sublayer(self.norm(x))))
 
-
-"""
-Decoder 블록은 FeedForward 레이어와 MultiHead 어텐션, Masked Multihead 어텐션 레이어를 가진다.
-MaskedMultiHeadAttention -> MultiHeadAttention(encoder-decoder attention) -> FeedForward
-"""
 class Decoder(nn.Module):
   def __init__(self, d_model,head_num, dropout):
     super(Decoder,self).__init__()
