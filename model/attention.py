d86bf142871393b884194e06b2532d03e208ee07
nawnoes <nawnoes@gmail.com>
2021-08-06 17:33:31 +0900

    sparse attention

--- model/attention.py
+++ model/attention.py
@@ -83,4 +83,45 @@ class BlockSparseAttention(nn.Module):
         return outputs
 
     @staticmethod
-    def
+    def torch_bmm_nd(input_1, input_2, dim= None):
+        """
+        Fast nd matrix multiplication
+        """
+        return torch.bmm(input_1.reshape((-1,) + input_1.shape[-2:]), input_2.reshape((-1,) + input_2.shape[-2:])).view(
+            input_1.shape[:dim-2] + (input_1.shape[dim-2], input_2.shape[dim-2])
+        )
+
+    @staticmethod
+    def torch_bmm_nd_transpose(input_1, input_2, dim=None):
+        """
+        Fast nd matrix multiplication with transpose
+        """
+        return torch.bmm(input_1.reshape((-1,) + input_1.shape[-2:]), input_2.reshape((-1,) + input_2.shape[-2:]).transpose(1,2)
+                         ).view(input_1.shape[:dim-2] + (input_1.shape[dim-2], input_2.shape[dim-2]))
+
+    def block_sparse_attention(self,
+                                query_layer,
+                                key_layer,
+                                value_layer,
+                                band_mask,
+                                from_mask,
+                                to_mask,
+                                from_blocked_mask,
+                                to_blocked_mask,
+                                num_attention_heads,
+                                num_rand_blocks,
+                                attention_head_size,
+                                from_block_size,
+                                to_block_size,
+                                batch_size,
+                                from_seq_length,
+                                to_seq_length,
+                                seed=None,
+                                plan_from_length=None,
+                                plan_num_rand_blocks=None,
+                                output_attentions=None
+                               ):
+        """
+        block sparse attention as suggested in BigBird paper
+
+        """
